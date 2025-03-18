5e8dd53dbf346c1046fceaa74fec099faa738de5
Dilson <dilsono316@gmail.com>
2025-03-18 12:12:07 -0300

    feat: navbar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
