8ad8712198d4d631da91030b96cd97d18561faff
Dilson <dilsono316@gmail.com>
2025-03-18 14:56:00 -0300

    feat: service rick and morty

--- src/app/core/services/rick_and_morty/rick-and-morty-service.service.ts
+++ src/app/core/services/rick_and_morty/rick-and-morty-service.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Character } from '../../../shared/models/Character';
+import { environment } from '../../../config/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickAndMortyServiceService {
 
-  constructor() { }
+  constructor(private request: HttpClient) { }
+
+  private url: string = environment.api;
+
+  getAll(): Observable<Character[]>{
+    return this.request.get<Character[]>(`${this.url}/character`);
+  }
 }
---
+++ src/app/shared/models/Character.ts
@@ -0,0 +1,8 @@
+export class Character {
+    public id: string = '';
+    public name: string = '';
+    public status: string = '';
+    public location: string = '';
+    public image: string = '';
+    public qtdEpisodes: string = '';
+}
