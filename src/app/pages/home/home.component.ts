44b1ef58521e500c88d90a79ade216756f213aff
Dilson <dilsono316@gmail.com>
2025-03-18 13:36:26 -0300

    feat: init character section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { CharacterComponent } from '../../shared/components/character/character.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent,NavbarComponent],
+  imports: [HeaderComponent,NavbarComponent,CharacterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
