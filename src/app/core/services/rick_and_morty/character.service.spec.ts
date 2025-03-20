4384a17802d0172b9fc909ef7971e7a89e932995
Dilson <dilsono316@gmail.com>
2025-03-20 14:59:09 -0300

    test: testing character service

--- src/app/core/services/rick_and_morty/character.service.spec.ts
+++ src/app/core/services/rick_and_morty/character.service.spec.ts
@@ -2,10 +2,14 @@ import { TestBed } from '@angular/core/testing';
 
 import { CharacterService } from './character.service';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { ApiResponse } from '../../../shared/models/ApiResponse';
+import { environment } from '../../../../environments/environment.development';
+import { TypeSearch } from '../../../shared/enums/TypeSearch';
 
 describe('CharacterService', () => {
   let service: CharacterService;
+  let  httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,9 +19,61 @@ describe('CharacterService', () => {
       ]
     });
     service = TestBed.inject(CharacterService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get for all characters (getAll)', () => {
+    const mockResponse: ApiResponse = {
+      info: { count: 826, pages: 42, next: '', prev: '' },
+      results: [{ id: '1', name: 'Rick Sanchez', status: 'Alive', location: { name: 'Earth' }, image: '', episode: [] }]
+    };
+
+    service.getAll(1).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}/character?page=1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse); 
+  });
+
+  it('should search for characters by name (getBySearch)', () => {
+    const mockResponse: ApiResponse = {
+      info: { count: 1, pages: 1, next: '', prev: '' },
+      results: [{ id: '1', name: 'Morty Smith', status: 'Alive', location: { name: 'Earth' }, image: '', episode: [] }]
+    };
+
+    service.getBySearch('Morty', TypeSearch.NAME).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne(`${environment.api}/character?name=Morty`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should search for characters by status (getBySearch)', () => {
+    const mockResponse: ApiResponse = {
+      info: { count: 3, pages: 1, next: '', prev: '' },
+      results: [
+        { id: '1', name: 'Rick Sanchez', status: 'Alive', location: { name: 'Earth' }, image: '', episode: [] },
+        { id: '2', name: 'Morty Smith', status: 'Alive', location: { name: 'Earth' }, image: '', episode: [] }
+      ]
+    };
+  
+    service.getBySearch('Alive', TypeSearch.STATUS).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+      expect(response.results.length).toBe(2);
+      expect(response.results[0].status).toBe('Alive'); 
+    });
+  
+    const req = httpMock.expectOne(`${environment.api}/character?status=Alive`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+  
 });
