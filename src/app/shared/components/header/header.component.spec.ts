16515f3e59530b20879a4692e7564dfa73e1b901
Dilson <dilsono316@gmail.com>
2025-03-20 17:14:06 -0300

    teste: search component

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -20,4 +20,11 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Descubra o melhor de Rick And Morty Show');
+  });
 });
