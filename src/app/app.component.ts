5c1c900dd833f7984b0c43bbe7164c314f2aa5a4
Dilson <dilsono316@gmail.com>
2025-03-22 11:52:08 -0300

    refact: header props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/layouts/navbar/navbar.component';
-import { HeaderComponent } from './core/layouts/header/header.component';
 import { FooterComponent } from './core/layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/character/list-character/list-character.component.ts
+++ src/app/pages/character/list-character/list-character.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CharacterComponent } from '../../../shared/components/character/character.component';
+import { HeaderComponent } from '../../../core/layouts/header/header.component';
 
 @Component({
   selector: 'app-list-character',
-  imports: [CharacterComponent],
+  imports: [CharacterComponent,HeaderComponent],
   templateUrl: './list-character.component.html',
   styleUrl: './list-character.component.scss'
 })
