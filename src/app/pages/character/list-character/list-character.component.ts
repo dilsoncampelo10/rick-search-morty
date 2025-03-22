0067574f0ce3f7b73e635d21d6cdf0c64f96cbd7
Dilson <dilsono316@gmail.com>
2025-03-22 10:26:45 -0300

    style: paginations buttons ands modal content

--- src/app/pages/character/list-character/list-character.component.ts
+++ src/app/pages/character/list-character/list-character.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../../core/layouts/navbar/navbar.component';
-import { HeaderComponent } from '../../../core/layouts/header/header.component';
 import { CharacterComponent } from '../../../shared/components/character/character.component';
-import { FooterComponent } from '../../../core/layouts/footer/footer.component';
 
 @Component({
   selector: 'app-list-character',
-  imports: [HeaderComponent,NavbarComponent,CharacterComponent, FooterComponent],
+  imports: [CharacterComponent],
   templateUrl: './list-character.component.html',
   styleUrl: './list-character.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../core/layouts/header/header.component';
-import { NavbarComponent } from '../../core/layouts/navbar/navbar.component';
-import { CharacterComponent } from '../../shared/components/character/character.component';
-import { FooterComponent } from '../../core/layouts/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent,NavbarComponent, FooterComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
