0785734fea4de391fbcc207edf3e33a49d2e8206
Dilson <dilsono316@gmail.com>
2025-03-22 13:11:07 -0300

    test: retest header component

--- src/app/core/layouts/header/header.component.spec.ts
+++ src/app/core/layouts/header/header.component.spec.ts
@@ -22,9 +22,18 @@ describe('HeaderComponent', () => {
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
+    component.title = 'Descubra o melhor de Rick And Morty Show';
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Descubra o melhor de Rick And Morty Show');
+  });
+
+  it('should set background image style', () => {
+    const testImageUrl = 'https://images7.alphacoders.com/133/1335145.jpg';
+    component.image = testImageUrl;
+    fixture.detectChanges();
+
+    const headerElement = fixture.nativeElement.querySelector('header') as HTMLElement;
+    expect(headerElement.style.backgroundImage).toContain(`url("${testImageUrl}")`);
   });
 });
