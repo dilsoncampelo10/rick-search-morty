93b7ed42d324bd48b193415703564154d7e1f58a
Dilson <dilsono316@gmail.com>
2025-03-23 15:25:50 -0300

    test: test info boz

--- src/app/shared/components/info-box/info-box.component.spec.ts
+++ src/app/shared/components/info-box/info-box.component.spec.ts
@@ -20,4 +20,20 @@ describe('InfoBoxComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the title correctly', () => {
+    component.title = 'Total de personagens';
+    fixture.detectChanges();
+    const cardHeader = fixture.nativeElement.querySelector('.card-header');
+    expect(cardHeader.textContent).toContain('Total de personagens');
+  });
+
+  it('should display the total correctly', () => {
+    component.total = '100';
+    fixture.detectChanges();
+    const totalValue = fixture.nativeElement.querySelector('.card-title');
+    expect(totalValue.textContent.trim()).toBe('100');
+  });
+
+
 });
